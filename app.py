9b9a7e379195508ffc5b8def7e2156eec912deb9
22240 <22240@wc.school.nz>
2026-03-16 09:22:03 +1300

    added framework

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
+
+DATABASE = "C:/Users/22240/mini_project/tutor_db"
 
 app = Flask(__name__)
 
 
+def connect_database(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+        print("error has occurred while connecting to the database")
+    return
+
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def render_homepage():
+    return render_template('base.html')
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/dashboard')
+def render_dashboard_page():
+    con = connect_database(DATABASE)
+    query = "SELECT session_id, session_tutor, session_tutee, session_room, session_time FROM session_db"
+    cur = con.cursor()
+    cur.execute(query)
+    sessions = cur.fetchall()
+    print(sessions)
+    con.close()
+    return render_template('dashboard.html', session_data=sessions)
+
+
+@app.route('/contact')
+def render_contact_page():
+    return render_template('contact.html')
+
+
+app.run(host='0.0.0.0', debug=True)
